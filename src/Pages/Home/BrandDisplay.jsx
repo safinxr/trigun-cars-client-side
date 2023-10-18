ab0bef3c3a69cd78548c6f7a7e5861f4b1ae1033
safinxr <safinxr@gmail.com>
2023-10-18 19:31:57 +0600

    brand display part responsive done

--- src/Pages/Home/BrandDisplay.jsx
+++ src/Pages/Home/BrandDisplay.jsx
@@ -7,11 +7,10 @@ const BrandDisplay = () => {
             .then(res => res.json())
             .then(data => setBrands(data))
     }, [])
-    console.log(brands);
     return (
-        <div className='my-32 max-w-6xl mx-auto' >
+        <div className='my-32 max-w-6xl mx-auto px-3 mx:px-8 lg:px-0' >
             <h2 className='uppercase text-center text-5xl font-semibold mb-16'>Car Brands</h2>
-            <div className='grid grid-cols-1 md:gird-cols-2 lg:grid-cols-4 gap-8 '>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 '>
                 {
                     brands.map(brand => <BrandCard brand={brand} key={brand.id}></BrandCard>)
                 }
@@ -27,10 +26,10 @@ const BrandCard = ({ brand }) => {
     const { brand_name, brand_img, car_img } = brand;
     return (
         <div>
-            <div className="card bg-base-100 card-shadow duration-300  active:scale-95 relative group">
+            <div className="card bg-base-100 card-shadow duration-300  active:scale-95 relative group ">
                 <figure>
                     <img src={brand_img} alt="brand" />
-                    <img className='absolute opacity-0 group-hover:opacity-100 rounded-t-2xl duration-500' src={car_img} alt="car" />
+                    <img className='hidden lg:flex absolute opacity-0 group-hover:opacity-100 rounded-t-2xl duration-500 ' src={car_img} alt="car" />
                 </figure>
                 <div className="card-body bg-black rounded-b-2xl p-2">
                     <h2 className="text-white text-center font-bold">{brand_name}</h2>
