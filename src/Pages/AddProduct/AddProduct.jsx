87564bc6b80a6a0729dc0df142feacaa74a3a2bf
safinxr <safinxr@gmail.com>
2023-10-21 00:28:11 +0600

    add car page color change

--- src/Pages/AddProduct/AddProduct.jsx
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -50,14 +50,14 @@ const AddProduct = () => {
     return (
         <div className='bg-[url("https://images.unsplash.com/photo-1586375103528-8bc247aa8110?auto=format&fit=crop&q=80&w=1528&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-cover bg-center bg-no-repeat bg-blend-overlay bg-black bg-opacity-50'>
             <div className='max-w-6xl mx-auto px-3 md:px-8 lg:px-o text-roboto py-12 md:py-20 lg:py-24'>
-                <div className='lg:w-3/4 mx-auto form-shadow p-6 md:p-8 lg:p-16 rounded-2xl bg-white'>
-                    <h1 className='text-center font-bold text-2xl mb-12'>Add Your Car</h1>
+                <div className='lg:w-3/4 mx-auto form-shadow p-6 md:p-8 lg:p-16 rounded-2xl bg-primary'>
+                    <h1 className='text-center text-white font-bold text-3xl mb-12'>Add Your Car</h1>
                     <form onSubmit={addHandel}
                         className='grid grid-cols-1 md:grid-cols-2 gap-8' >
                         {/* Name */}
-                        <input required className="w-full input input-bordered input-error h-10 focus:border-0" type="text" name='name' placeholder='Name' />
+                        <input required className="w-full input input-bordered  h-10 focus:border-0" type="text" name='name' placeholder='Name' />
                         {/* brandName */}
-                        <select required className="w-full input input-bordered input-error h-10 focus:border-0" name="brandName" >
+                        <select required className="w-full input input-bordered  h-10 focus:border-0" name="brandName" >
                             <option value="toyota">Toyota</option>
                             <option value="honda">Honda</option>
                             <option value="ford">Ford</option>
@@ -67,14 +67,14 @@ const AddProduct = () => {
                             <option value="audi">Audi</option>
                         </select>
                         {/* type */}
-                        <input required className="w-full input input-bordered input-error h-10 focus:border-0" type="text" name='type' placeholder='Type' />
+                        <input required className="w-full input input-bordered  h-10 focus:border-0" type="text" name='type' placeholder='Type' />
                         {/* price  */}
-                        <input required className="w-full input input-bordered input-error h-10 focus:border-0" type="number" name='price' placeholder='Price' />
+                        <input required className="w-full input input-bordered  h-10 focus:border-0" type="number" name='price' placeholder='Price' />
                         {/* img  */}
-                        <input required className="w-full input input-bordered input-error h-10 focus:border-0" type="text" name='img' placeholder='Image URL' />
+                        <input required className="w-full input input-bordered  h-10 focus:border-0" type="text" name='img' placeholder='Image URL' />
                         {/* rating */}
                         <select required
-                            className="w-full input input-bordered input-error h-10 focus:border-0" name="rating"
+                            className="w-full input input-bordered  h-10 focus:border-0" name="rating"
 
                         >
                             <option value="1">1 Star</option>
@@ -86,11 +86,11 @@ const AddProduct = () => {
                         </select>
 
                         {/* description  */}
-                        <textarea required className="md:col-span-2 w-full input input-bordered input-error h-44 focus:border-0" name="description" placeholder='Write short description' cols="30" rows="10"></textarea>
-                        <button type='submit' className="rounded-md px-10 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-[#FF4D24]  mx-auto md:col-span-2">
+                        <textarea required className="md:col-span-2 w-full input input-bordered  h-44 focus:border-0" name="description" placeholder='Write short description' cols="30" rows="10"></textarea>
+                        <button type='submit' className="rounded-md px-10 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-white  mx-auto md:col-span-2">
 
-                            <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-[#FF4D24] top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-                            <span className="relative text-[#FF4D24] transition duration-300 group-hover:text-white ease">Submit
+                            <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-white top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+                            <span className="relative text-white transition duration-300 group-hover:text-black ease">Submit
                             </span>
                         </button>
                     </form>
