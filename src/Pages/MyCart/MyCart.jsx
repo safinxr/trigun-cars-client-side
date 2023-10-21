809677fcdb79a8e7091d8c1cb7abcd35c419c887
safinxr <safinxr@gmail.com>
2023-10-21 18:06:38 +0600

    delete button added

--- src/Pages/MyCart/MyCart.jsx
+++ src/Pages/MyCart/MyCart.jsx
@@ -3,6 +3,7 @@ import { ContextAuth } from '../../Context/Context';
 import { BsStarFill } from 'react-icons/bs';
 import { AiOutlineDelete } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2'
 
 const MyCart = () => {
     const { user } = useContext(ContextAuth)
@@ -36,7 +37,40 @@ export default MyCart;
 
 
 const CartCard = ({ cart }) => {
-    const { name, brand_name, type, description, price, img, rating, cart_id } = cart;
+    const { name, brand_name, type, description, price, img, rating, cart_id, _id } = cart;
+
+    const deleteButton = ()=>{
+        Swal.fire({
+            title: 'Are you sure?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:3000/cart/${_id}`, {
+                    method: "DELETE",
+
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                        console.log('delete');
+
+                        // const remaining = carts.filter(coffee => coffee._id !== id)
+                        // setCoffees(remaining)
+                        // console.log(remaining);
+                    })
+
+            }
+        })
+    }
+
     return (
         <div className="card md:card-side bg-base-100 card-shadow hover:scale-105 duration-300">
             <figure className=' md:w-2/5'><img className='w-full h-full' src={img} alt="Movie" /></figure>
@@ -55,7 +89,7 @@ const CartCard = ({ cart }) => {
                         <span className="absolute flex items-center justify-center w-full h-full text-black transition-all duration-300 transform group-hover:translate-x-full ease uppercase">Details</span>
                         <span className="relative invisible uppercase">Details</span>
                     </Link>
-                    <button  className="relative inline-flex items-center justify-center p-4 px-6 py-1.5 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-[#FF4D24] rounded-md shadow-md group">
+                    <button onClick={deleteButton}  className="relative inline-flex items-center justify-center p-4 px-6 py-1.5 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-[#FF4D24] rounded-md shadow-md group">
                         <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-[#FF4D24] group-hover:translate-x-0 ease">
                             <AiOutlineDelete className='font-bold text-2xl'></AiOutlineDelete>
                         </span>
