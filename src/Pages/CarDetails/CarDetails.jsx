5aaec00b641581324ec144962a6e94f6821783fa
safinxr <safinxr@gmail.com>
2023-10-21 12:36:58 +0600

    add to cart button edit

--- src/Pages/CarDetails/CarDetails.jsx
+++ src/Pages/CarDetails/CarDetails.jsx
@@ -26,10 +26,11 @@ const CarDetails = () => {
                     </div>
                     <button  className="my-8 relative inline-flex items-center justify-center p-4 px-6 py-1.5 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-[#FF4D24] rounded-md shadow-md group">
                         <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-[#FF4D24] group-hover:translate-x-0 ease">
-                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                            <img className="w-6 h-6" src="https://i.ibb.co/r584Mpr/add-xxl.png" alt="" />
+                            
                         </span>
-                        <span className="absolute flex items-center justify-center w-full h-full text-black transition-all duration-300 transform group-hover:translate-x-full ease uppercase">Details</span>
-                        <span className="relative invisible uppercase">Details</span>
+                        <span className="absolute flex items-center justify-center w-full h-full text-black transition-all duration-300 transform group-hover:translate-x-full ease uppercase">Add to cart</span>
+                        <span className="relative invisible uppercase">Add to cart</span>
                     </button>
                 </div>
             </div>
